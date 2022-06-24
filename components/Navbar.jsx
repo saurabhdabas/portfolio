55409445492021aa2729a1727b91e4b05f5af3b6
Saurabh Dabas <developerdabas@gmail.com>
2022-06-24 10:34:04 +0530

    Add styling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
       
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
