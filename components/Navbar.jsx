9dd60ef12540342211b7accb047ce3e11111b4d2
Saurabh Dabas <developerdabas@gmail.com>
2022-06-24 16:11:46 +0530

    Add navbar items and display harmburger menu for small media screens

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React,{useState} from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai';
 
 const Navbar = () => {
+  const [linkColor, setLinkColor] = useState('#000');
+  const [navbar, setNavbar] = useState(false);
+  const handleNavigationBar = () => {
+    setNavbar(true);
+  }
   return (
-    <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-      
+    <div className='fixed w-full h-20 z-[100] bg-[#FFF]'>
+      <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+        <div className='flex justify-between items-center p-2 text-[#DC3545] text-xl '>
+          <span className="">
+            <Image src="/../public/assets/s.png" width="50px" height="50px" alt=""/>
+          </span>
+          <div>
+            <ul style={{ color: `${linkColor}` }} className='hidden md:flex'>
+              <li className='ml-20 text-lg hover:bg-gray-100 p-2 rounded-lg'>
+                <Link href='/'>Home</Link>
+              </li>
+              <li className='ml-10 text-lg  hover:bg-gray-100 p-2 rounded-lg'>
+                <Link href='/#about'>About</Link>
+              </li>
+              <li className='ml-10 text-lg  hover:bg-gray-100 p-2 rounded-lg'>
+                <Link href='/#skills'>Skills</Link>
+              </li>
+              <li className='ml-10 text-lg  hover:bg-gray-100 p-2 rounded-lg'>
+                <Link href='/#projects'>Projects</Link>
+              </li>
+              <li className='ml-10 text-lg  hover:bg-gray-100 p-2 rounded-lg'>
+                <Link href='/#contact'>Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      <div>
+      {/* Add Hamburger Icon to be displayed for medium screen size*/}
+      <div
+        style={{ color: `${linkColor}` }}
+        onClick = {handleNavigationBar}
+        className='md:hidden'
+        >
+        <AiOutlineMenu size={25} />
+      </div>
+      <button type="button" aria-label="dark mode" className="shadow hidden md:block self-center bg-white hover:bg-gray-100 focus:ring focus:ring-gray-500  dark:bg-black dark:hover:bg-gray-900 focus:outline-none rounded-md p-1.5"><svg aria-label="moon" width="18" height="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="fill-current text-gray-700"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg></button>
+      </div>
+      </div>
     </div>
   )
 }
