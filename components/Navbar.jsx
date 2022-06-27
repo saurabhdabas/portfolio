9edb76e54da89dc2af8677bca2af3d2b9b37da13
Saurabh Dabas <developerdabas@gmail.com>
2022-06-27 16:49:11 +0530

    Change text styling unde dark mde

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,9 +13,9 @@ const Navbar = ({theme,setTheme}) => {
   return (
     <div className='dark:bg-black  fixed w-full h-20 z-[100] bg-[#F6F6F6]'>
       <div className='flex justify-between items-center w-full h-full 2xl:px-32'>
-        <div className='flex justify-between items-center p-2 text-[#DC3545] text-xl '>
+        <div className='flex justify-between items-center p-2 text-[#DC3545] text-xl'>
           <span className="">
-            <Image src="/../public/assets/s.png" width="50px" height="50px" alt=""/>
+            <Image src="/../public/assets/new.png" width="90px" height="70px" alt=""/>
           </span>
           <div>
             <ul  className='hidden md:flex dark:text-[#999999] text-black'>
@@ -48,7 +48,6 @@ const Navbar = ({theme,setTheme}) => {
           <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')} type="button" aria-label="dark mode" className="shadow md:block self-center bg-white hover:bg-gray-100 focus:ring focus:ring-gray-500  dark:bg-black dark:hover:bg-gray-900 focus:outline-none rounded-md p-1.5">
           {theme === 'dark' ? <svg aria-label="sun" width="18" height="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="fill-current text-gray-50"><path fill-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd"></path></svg>
           : <svg aria-label="moon" width="18" height="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="fill-current text-gray-500"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>  }
-          
           </button>
         </div>
       </div>
