0fb489329d148d6af2871bcbe74939ecd6b86f09
Saurabh Dabas <developerdabas@gmail.com>
2022-06-29 02:24:43 +0530

    Add nodemailer for contact form submission

---
+++ pages/api/mail.js
@@ -0,0 +1,39 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function (req, res) {
+  require('dotenv').config();
+  const nodemailer = require('nodemailer');
+  
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.username,
+      pass: process.env.password,
+      clientId: process.env.clientID,
+      clientSecret: process.env.clientSecret,
+      refreshToken: process.env.refreshToken
+    }
+  });
+
+  const mailConfigurations = {
+  
+    // It should be a string of sender email
+    from: process.env.username,
+      
+    // Comma Separated list of mails
+    to: 'saurabhdabas0@gmail.com',
+  
+    // Subject of Email
+    subject: `Message is from ${req.body.name}`,
+      
+    // This would be the text of email body
+    text: `${req.body.message}`
+  };
+
+  transporter.sendMail(mailConfigurations, function(error, info){
+    if (error) throw Error(error);
+       console.log('Email Sent Successfully');
+    console.log(info);
+  });
+}
