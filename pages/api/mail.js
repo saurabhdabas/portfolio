675bcfc99bdd7ecdfb6cb81c7e03b3e2ae73f441
Saurabh Dabas <developerdabas@gmail.com>
2022-07-06 20:10:06 +0530

    Fix nodemailer issues

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -4,7 +4,7 @@ export default function mail(req, res) {
   require('dotenv').config();
   if (req.method === 'POST') {
     // Process a POST request
-     res.send(req.body);
+    res.status(200).json(req.body)
   }
   
   const nodemailer = require('nodemailer');
