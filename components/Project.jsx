4006c29ad4954f290cbd85dc8a6153fde92b5bf5
Saurabh Dabas <developerdabas@gmail.com>
2022-09-28 14:57:11 -0600

    Fix width for projects

--- components/Project.jsx
+++ components/Project.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Project = ({title, backgroundImg, tech, projectUrl}) => {
   return (
-    <div className='relative flex items-center justify-center h-auto w-full shadow-md shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-red-500 to-red-800'>
+    <div className='relative flex items-center justify-center h-auto w-11/12 shadow-md shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-red-500 to-red-800'>
     <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='/' width="400px" height="250px" /> 
     <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
